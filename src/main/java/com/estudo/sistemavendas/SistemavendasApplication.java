b98a7e4e5d1d392f3b048b6ed683ff288fcca233
Renato <rnt.dsantos@gmail.com>
2026-06-12 08:15:24 -0300

    first commit

---
+++ src/main/java/com/estudo/sistemavendas/SistemavendasApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.sistemavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavendasApplication.class, args);
+	}
+
+}
